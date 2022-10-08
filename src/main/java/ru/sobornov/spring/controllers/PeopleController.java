4d73386b68e0f13facde6fff6c33899bcb3baa1d
Vladimir Sobornov <thecodegod1@gmail.com>
2022-10-08 13:42:44 +0300

    Initial commit

---
+++ src/main/java/ru/sobornov/spring/controllers/PeopleController.java
@@ -0,0 +1,11 @@
+package ru.sobornov.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author : Sobornov Vladimir
+ * @since : 08.10.2022
+ **/
+@Controller
+public class PeopleController  {
+}
